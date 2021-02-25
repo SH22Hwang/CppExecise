838350b802e0682ee28b0956cbfe9f6b4ee4c52f
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-02-25 17:34:41 +0900

    오류 발생...?

--- chapter4/Q04_3_1.cpp
+++ chapter4/Q04_3_1.cpp
@@ -1,58 +1,66 @@
-#include <iostream>
-using namespace std;
-
-class Point {
-private:
-	int xpos, ypos;
-
-public:
-	void Init(int x, int y) {
-		xpos = x;
-		ypos = y;
-	}
-
-	void ShowPointInfo() const {
-		cout << "[" << xpos << "," << ypos << "]" << endl;
-	}
-};
-
-class Circle {
-private:
-	Point center;
-	double radius;
-
-public:
-	void Init(int x, int y, double num) {
-		center.Init(x, y);
-		radius = num;
-	}
-
-	void ShowCircleInfo() const {
-		cout << "radius: " << radius << endl;
-		center.ShowPointInfo();
-	}
-};
-
-class Ring {
-private:
-	Circle inner, outer;
-public:
-	void Init(int xIn, int yIn, double radiusIn, int xOut, int yOut, double radiusOut) {
-		inner.Init(xIn, yIn, radiusIn);
-		outer.Init(xOut, yOut, radiusOut);
-	}
-
-	void ShowRingInfo() const {
-		cout << "Inner Circle Info..." << endl;
-		inner.ShowCircleInfo();
-		cout << "Outer Circle Info..." << endl;
-		outer.ShowCircleInfo();
-	}
-};
-
+//#include <iostream>
+//using namespace std;
+//
+//class Point {
+//private:
+//	int xpos, ypos;
+//
+//public:
+//	Point(int x, int y)
+//		: xpos(x), ypos(y) {}
+//
+//	//void Init(int x, int y) {
+//	//	xpos = x;
+//	//	ypos = y;
+//	//}
+//
+//	void ShowPointInfo() const {
+//		cout << "[" << xpos << "," << ypos << "]" << endl;
+//	}
+//};
+//
+//class Circle {
+//private:
+//	Point center;
+//	double radius;
+//
+//public:
+//	Circle(int x, int y, double num)
+//		:center(x, y), radius(num) {}
+//
+//	//void Init(int x, int y, double num) {
+//	//	center.Init(x, y);
+//	//	radius = num;
+//	//}
+//
+//	void ShowCircleInfo() const {
+//		cout << "radius: " << radius << endl;
+//		center.ShowPointInfo();
+//	}
+//};
+//
+//class Ring {
+//private:
+//	Circle inner, outer;
+//public:
+//	Ring(int xIn, int yIn, double radiusIn, int xOut, int yOut, double radiusOut) 
+//		:inner(xIn, yIn, radiusIn), outer(xOut, yOut, radiusOut) {}
+//
+//	//void Init(int xIn, int yIn, double radiusIn, int xOut, int yOut, double radiusOut) {
+//	//	inner.Init(xIn, yIn, radiusIn);
+//	//	outer.Init(xOut, yOut, radiusOut);
+//	//}
+//
+//	void ShowRingInfo() const {
+//		cout << "Inner Circle Info..." << endl;
+//		inner.ShowCircleInfo();
+//		cout << "Outer Circle Info..." << endl;
+//		outer.ShowCircleInfo();
+//	}
+//};
+//
 //int main(void) {
-//	Ring ring;
-//	ring.Init(1, 1, 4, 2, 2, 9);
+//	Ring ring(1, 1, 4, 2, 2, 9);
 //	ring.ShowRingInfo();
 //	return 0;
 //}
